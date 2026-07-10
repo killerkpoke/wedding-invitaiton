124ed0655f2ef7bbb44ad88bfd3692ee8715c8e1
kpoke <killerkpoke@gmail.com>
2026-07-10 14:04:01 +0200

    remove "PM" on timeline

--- components/ScheduleTimeline.tsx
+++ components/ScheduleTimeline.tsx
@@ -3,13 +3,13 @@
 import ScheduleCard from "@/components/ScheduleCard";
 
 const timeline = [
-  { time: "14:00 PM", mainEvent: "Vendégérkezés", description: "Italozás és harapnivalók" },
-  { time: "15:00 PM", mainEvent: "Ceremónia", description: "" },
-  { time: "16:00 PM", mainEvent: "Gratulációk", description: "pezsgős koccintás és csoportképek" },
-  // { time: "17:00 PM", mainEvent: "TBD", description: "Programok, játékok" },
-  { time: "19:00 PM", mainEvent: "Köszöntők és vacsora", description: "" },
-  { time: "20:30 PM", mainEvent: "Torta vágás", description: "" },
-  { time: "12:00 AM", mainEvent: "Éjféli vacsora és Buli", description: "" },
+  { time: "14:00", mainEvent: "Vendégérkezés", description: "Italozás és harapnivalók" },
+  { time: "15:00", mainEvent: "Ceremónia", description: "" },
+  { time: "16:00", mainEvent: "Gratulációk", description: "pezsgős koccintás és csoportképek" },
+  // { time: "17:00", mainEvent: "TBD", description: "Programok, játékok" },
+  { time: "19:00", mainEvent: "Köszöntők és vacsora", description: "" },
+  { time: "20:30", mainEvent: "Torta vágás", description: "" },
+  { time: "00:00", mainEvent: "Éjféli vacsora és Buli", description: "" },
 ];
 
 export default function ScheduleTimeline() {
