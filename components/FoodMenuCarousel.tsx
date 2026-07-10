f98d549c8f03782e742baf4ab6168090527127f2
kpoke <killerkpoke@gmail.com>
2026-07-10 14:43:01 +0200

    formatting on carousel

--- components/FoodMenuCarousel.tsx
+++ components/FoodMenuCarousel.tsx
@@ -90,11 +90,17 @@ export default function FoodMenuCarousel() {
 
             {/* LEFT / RIGHT ARROWS */}
             <div className="absolute left-5 right-5 top-[3.5%] md:top-[5%] lg:top-[10%] flex -translate-y-1/2 transform justify-between">
-              <a href={`#${prev}`} className="btn btn-circle bg-[#9F2D00] border-[#9F2D00] btn-sm opacity-70 hover:opacity-100">
+              <a
+                href={`#${prev}`}
+                className="btn btn-circle bg-[#9F2D00] border-[#9F2D00] btn-sm opacity-70 hover:opacity-100"
+              >
                 ❮
               </a>
 
-              <a href={`#${next}`} className="btn btn-circle bg-[#9F2D00] border-[#9F2D00] btn-sm opacity-70 hover:opacity-100">
+              <a
+                href={`#${next}`}
+                className="btn btn-circle bg-[#9F2D00] border-[#9F2D00] btn-sm opacity-70 hover:opacity-100"
+              >
                 ❯
               </a>
             </div>
